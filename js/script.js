389a4c06c5246f94a56002297038ffecfedb0565
sohanur <sohanuractive007@gmail.com>
2025-03-02 02:08:19 +0600

    side bar added the box title on clike function

--- js/script.js
+++ js/script.js
@@ -4,21 +4,73 @@ document.getElementById("colorBtn").addEventListener("click", function () {
   document.body.style.backgroundColor = color;
 });
 
-
+// redirect blog page 
+document.getElementById("discover").addEventListener("click", function () {
+  window.location.href = "/blog.html";
+});    
+ 
 // current zone
 function updateClock() {
-  document.getElementById("clock-zone").innerText = new Date().toLocaleTimeString();
+  document.getElementById("clock-zone").innerText =
+    new Date().toLocaleTimeString();
 }
 setInterval(updateClock, 1000);
 updateClock();
 
 
-// completed button 
-const buttons = document.querySelectorAll(".update")
+
+
+
+
+// completed button
+const buttons = document.querySelectorAll(".update");
 for (const btn of buttons) {
-    btn.addEventListener("click", function (event) {
-        alert("board updated successful");
-        event.target.classList.add("disabled")
-        event.target.disabled = true;
-    })
+  btn.addEventListener("click", function (event) {
+    alert("board updated successful");
+    event.target.classList.add("disabled");
+    event.target.disabled = true;
+
+    // task count decremental
+    let taskCount = parseInt(document.getElementById("task-count").innerText);
+    if (taskCount > 0) {
+      taskCount--;
+      //   console.log(taskCount);
+      document.getElementById("task-count").innerText = taskCount
+        .toString()
+        .padStart(2, "0");
+    }
+    if (taskCount === 0) {
+      alert("Congratulations! You have completed all the current tasks.");
+    }
+
+    // task count incremental
+    let taskCountUp = parseInt(document.getElementById("count-up").innerText);
+    if (taskCount < 23) {
+      taskCountUp++;
+      document.getElementById("count-up").innerText = taskCountUp
+        .toString()
+        .padStart(2, "0");
+      //   console.log("Current Task Count:", taskCountUp);
+    }
+
+   const newContent = document.getElementById("new-content");
+   if (newContent) {
+     const boxTitle = event.target
+       .closest(".col-span-3")
+       .querySelector(".box-title");
+     if (boxTitle) {
+       const newTitle = document.createElement("li"); 
+        const currentTime = new Date().toLocaleTimeString();
+        newTitle.innerText = `Title: ${boxTitle.innerText} - Added at: ${currentTime}`;
+       newContent.appendChild(newTitle);
+     }
+   } else {
+     console.error("Element with id 'new-content' not found!");
+   }
+
+  });
 }
+
+
+
+
